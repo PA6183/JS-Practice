84707df207e46f3a5994619aa2c2009aa1e6ebdb
PA6183 <43440404+PA6183@users.noreply.github.com>
2023-11-09 12:02:31 +0200

    Added HW.html and HomeWorks.js files

---
+++ HomeWorks.js
@@ -0,0 +1,54 @@
+// //Task 2
+// console.log("Alfyorova")
+
+// //Task 3
+// let var1 = "Polina"
+// let var2 = "QC"
+// console.log(var1, var2);
+// var1 = var2
+// console.log(var1, var2);
+
+// //Task 4
+// const employee = {
+//     firstAndLastName:"Polina Alfyorova",
+//     currentLengthOfService: 3,
+//     isActive: true,
+//     certificates: undefined,
+//     languageCorseCertificate: null
+// }
+
+// //Task 5
+// let age=confirm("Have you reached the age of majority?")
+// let isAdult = age;
+// console.log(isAdult);
+
+// //Task 6
+// let firstName = "Polina";
+// let lastName = "Alfyorova";
+// let studyGroup = "JavaScript for TA [Q4, 2023]";
+// let yearOfBirth = 1981;
+// let isMarried = false;
+// console.log(typeof firstName);
+// console.log(typeof lastName);
+// console.log(typeof studyGroup);
+// console.log(typeof yearOfBirth);
+// console.log(typeof isMarried);
+
+// var nullType = null;
+// var undefinedType;
+// console.log(typeof nullType);
+// console.log(typeof undefinedType);
+
+// //Task 7
+// let login = prompt("Please enter your login");
+// let email = prompt("Please enter your email address");
+// let password = prompt("Please enter your password");
+// console.log(`Dear ${login}, your email is ${email} and your password is ${password}`);
+
+//Task 8
+let sec_per_hour = 60;
+let sec_per_day = 60*24;
+let days_per_month = prompt("Please enter number of days in a month")
+let sec_per_month = days_per_month*sec_per_day;
+console.log(`Seconds in 1 hour: ${sec_per_hour} \nSeconds in 1 day: ${sec_per_day} \nSeconds in a month:${sec_per_month}`);
+
